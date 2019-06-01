a598f55744e4dd1017d92718576a053e8a528eb5
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-06-01 03:12:12 +0100

    Created and used first model with a database

---
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
